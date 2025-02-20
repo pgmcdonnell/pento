667e5df386bb9d6dc21ec36f56d29fe3ac089802
Patrick McDonnell <patrick.g.mcdonnell@gmail.com>
2025-02-20 17:21:14 +0000

    Add Product image uploads

--- lib/pento_web/live/product_live/form_component.ex
+++ lib/pento_web/live/product_live/form_component.ex
@@ -23,22 +23,40 @@ defmodule PentoWeb.ProductLive.FormComponent do
         <.input field={@form[:description]} type="text" label="Description" />
         <.input field={@form[:unit_price]} type="number" label="Unit price" step="any" />
         <.input field={@form[:sku]} type="number" label="Sku" />
+        <div phx-drop-target={@uploads.image.ref}>
+          <.label>Image</.label>
+          <.live_file_input upload={@uploads.image} />
+        </div>
         <:actions>
           <.button phx-disable-with="Saving...">Save Product</.button>
         </:actions>
       </.simple_form>
+      <%= for image <- @uploads.image.entries do %>
+        <div class="mt-4">
+          <.live_img_preview entry={image} width="60" />
+          <progress value={image.progress} max="100" />
+        </div>
+      <% end %>
     </div>
     """
   end
 
   @impl true
   def update(%{product: product} = assigns, socket) do
-    {:ok,
-     socket
-     |> assign(assigns)
-     |> assign_new(:form, fn ->
-       to_form(Catalog.change_product(product))
-     end)}
+    changeset = Catalog.change_product(product)
+
+    socket =
+      socket
+      |> assign(assigns)
+      |> assign_form(changeset)
+      |> allow_upload(:image,
+        accept: ~w(.jpg .jpeg .png),
+        max_entries: 1,
+        max_file_size: 9_000_000,
+        auto_upload: true
+      )
+
+    {:ok, socket}
   end
 
   @impl true
@@ -51,7 +69,25 @@ defmodule PentoWeb.ProductLive.FormComponent do
     save_product(socket, socket.assigns.action, product_params)
   end
 
+  def params_with_image(socket, params) do
+    path =
+      socket
+      |> consume_uploaded_entries(:image, &upload_static_file/2)
+      |> List.first()
+
+    Map.put(params, "image_upload", path)
+  end
+
+  defp upload_static_file(%{path: path}, _entry) do
+    filename = Path.basename(path)
+    dest = Path.join("priv/static/images", filename)
+    File.cp!(path, dest)
+    {:ok, ~p"/images/#{filename}"}
+  end
+
   defp save_product(socket, :edit, product_params) do
+    product_params = params_with_image(socket, product_params)
+
     case Catalog.update_product(socket.assigns.product, product_params) do
       {:ok, product} ->
         notify_parent({:saved, product})
@@ -67,6 +103,8 @@ defmodule PentoWeb.ProductLive.FormComponent do
   end
 
   defp save_product(socket, :new, product_params) do
+    product_params = params_with_image(socket, product_params)
+
     case Catalog.create_product(product_params) do
       {:ok, product} ->
         notify_parent({:saved, product})
@@ -82,4 +120,8 @@ defmodule PentoWeb.ProductLive.FormComponent do
   end
 
   defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    assign(socket, :form, to_form(changeset))
+  end
 end
